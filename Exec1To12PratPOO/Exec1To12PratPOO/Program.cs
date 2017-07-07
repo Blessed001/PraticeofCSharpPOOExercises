47459bd696835e495ac4b9d055909d3a17ba4ba6
Blessed001 <claudiojoc46@mail.ru>
2017-07-07 13:13:04 +0300

    Created the class EstudantInf and property of this class, we also created method to calculate the final classification

---
+++ Exec1To12PratPOO/Exec1To12PratPOO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class EstudantInf //Create class EstudantInf 
+{
+    private string Nome;
+    private double Teste1;
+    private double Teste2;
+    public EstudantInf(string N, double T1, double T2) //Constructor of the class
+    {
+        Nome = N;
+        Teste1 = T1;
+        Teste2 = T2;
+    }
+    public double ClassFinal()// Calculing final classification 
+    {
+        return Math.Round((Teste1 + Teste2) / 2, 0, MidpointRounding.AwayFromZero);
+    }
+}
+
+
+namespace Exe1To12PratPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

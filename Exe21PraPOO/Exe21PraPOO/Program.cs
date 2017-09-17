a317bb9a1ecc9d4effe0eb44826ec2633e4cbc6e
diotec <claudiojoc46@mail.ru>
2017-09-17 11:33:27 +0300

    Class Amigo --> Sobreposicao do metodo ToString

---
+++ Exe21PraPOO/Exe21PraPOO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exe21PraPOO
+{
+    public class Amigo
+    {
+        private string Nome;
+        private string Apelido;
+        public Amigo(string N, string A)
+        {
+
+            Nome = N;
+            Apelido = A;
+
+        }
+        public override string ToString()
+        {
+            return Apelido + " , " + Nome;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Amigo A = new Amigo("Dionisio", "Chiquengue");
+            Console.WriteLine(A.ToString());
+            Console.ReadKey();
+        }
+    }
+}

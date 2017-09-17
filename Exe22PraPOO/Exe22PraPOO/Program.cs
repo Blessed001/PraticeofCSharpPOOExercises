4cbb064bf1c0199880a78f0207a1544eb38104ae
diotec <claudiojoc46@mail.ru>
2017-09-17 11:51:37 +0300

    Class RaizQuadrada --> Sobrecarga dos metodos RaizQ

---
+++ Exe22PraPOO/Exe22PraPOO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exe22PraPOO
+{
+    public class RaizQuad
+    {
+        public RaizQuad()
+        { }
+        public int RaizQ (int x)
+        {
+            return (int)Math.Sqrt(x);
+        }
+        public double RaizQ (double x)
+        {
+            return Math.Round(Math.Sqrt(x),2);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RaizQuad R = new RaizQuad();
+            int x = 49;
+            double y = 22.50;
+            Console.WriteLine("Raiz quadrada de {0} = {1}", x, R.RaizQ(x));
+            Console.WriteLine("Raiz quadrada de {0} = {1}", y, R.RaizQ(y));
+            Console.ReadKey();
+        }
+    }
+}
